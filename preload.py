564aeac717743a2671e421ed65a4d5cec68fb9a3
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2023-01-21 16:46:38 -0600

    changes configuration and dummy user creds

--- preload.py
+++ preload.py
@@ -4,7 +4,7 @@ from app.api.users.models import User
 async def load_users() -> None:
     user = User(
         username="tortellini",
-        password="70c07306f309b239d84ed87c17ca8d7c2b3c0955ad24f255cf102c626be9da64",
+        password="85b7ad22cd1825e5001d8bae3b2c89e2bd1647344298181fc9d62e46e60f51dc",
     )
 
     await user.save()
