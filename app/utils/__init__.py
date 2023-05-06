db14ba80894993891b9101abbaf122997908d605
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2023-05-06 13:40:04 -0500

    adds new function

---
+++ app/utils/__init__.py
@@ -0,0 +1,14 @@
+import json
+
+import requests
+
+
+def is_unique(s):
+    s = list(s)
+    s.sort()
+
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            return 0
+    else:
+        return 1
