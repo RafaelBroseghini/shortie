bd1bedeb54728bb7be5a0a642baf5b162b35e28c
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2023-05-14 21:53:24 -0500

    Update conn.py

--- app/cache/conn.py
+++ app/cache/conn.py
@@ -4,16 +4,14 @@ import redis
 
 from app.core.config import settings
 
-client = redis.StrictRedis(
-    decode_responses=True,
-    host=settings.REDIS_HOST,
-    password=settings.REDIS_PASSWORD,
-    port=settings.REDIS_PORT,
-)
-
 
 @contextmanager
 def RedisClientManager():
+    client = redis.StrictRedis(
+    decode_responses=True,
+    host=settings.REDIS_HOST,
+    password=settings.REDIS_PASSWORD,
+    port=settings.REDIS_PORT)
     try:
         yield client
     finally:
